12625f200e0c12115a443ed42f3e92c8530d3efa
pcarmo <pedro.esperanca.carmo@gmail.com>
2023-05-31 23:46:35 +0100

    facebook login popup showing up, but unable to access email data. ignoring this issue for now

--- ClientApp/src/components/Login.js
+++ ClientApp/src/components/Login.js
@@ -1,70 +1,22 @@
-
-import React, { Component } from 'react';
+import React from 'react';
 import FacebookLogin from 'react-facebook-login';
 
-
-const FACEBOOK_APP_ID = "1571786260014666";
-
-export class Login extends Component {
-    static displayName = Login.name;
-    componentDidMount() {
-
-        window.fbAsyncInit = function() {
-            console.log("running");
-            window.FB.init({
-                appId: FACEBOOK_APP_ID,
-                cookie: true,
-                xfbml: true,
-                version: 'v17.0'
-            });
-
-            window.FB.AppEvents.logPageView();
-
-        };
-
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) { return; }
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+export class Login extends React.Component {
+    responseFacebook(response) {
+        console.log(response);
+        console.log(response.email);
     }
 
     render() {
         return (
-            <div>
-                <h1>Login</h1>
-                <div>
-                    <FacebookLogin
-                        appId={FACEBOOK_APP_ID}
-                        autoLoad={false}
-                        fields="name,email,picture"
-                        onClick={test}
-                        callback={responseFacebook}
-                        onFailure={responseFailed}
-                    />
-                </div>
-            </div>
-        );
+            <FacebookLogin
+                appId="1571786260014666"
+                autoLoad={false}
+                fields="name,email"
+                scope="public_profile,email"
+                callback={this.responseFacebook}
+            />
+        )
     }
 }
-const test = () => {
-    console.log("working");
-}
-// Facebook login response handler
-const responseFacebook = (response) => {
-    console.log(response.Facebook);
 
-    // Perform any necessary actions with the Facebook response
-};
-
-const responseFailed = (response) => {
-
-    console.log("ERROR");
-    console.log(response.Facebook);
-
-    // Perform any necessary actions with the Facebook response
-};
-
-export default Login;
